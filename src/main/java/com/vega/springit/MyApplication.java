2d5e6f8d17cf120c2784c0a0d4fb420f1fdd752f
GKumar <GKumar@AE915R5S2.alico.corp>
2022-06-22 20:39:50 +0400

    Change to welcome message.

---
+++ src/main/java/com/vega/springit/MyApplication.java
@@ -0,0 +1,5 @@
+package com.vega.springit;
+
+public class MyApplication {
+
+}
--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -2,13 +2,18 @@ package com.vega.springit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.vega.springit.config.SpringitProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
-		System.out.println("Welcome to Springit!");
+		System.out.println("Feature: Welcome to Springit!");
 	}
 
 }
+
